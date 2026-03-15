1fec12e21f48b2e007e2e398a39389f0b18f67f3
Hélias Essabar <helias.essabar@epfedu.fr>
2026-03-15 22:23:07 +0100

    modif map dans les niveaux

--- TP4/src/js/niveau2.js
+++ TP4/src/js/niveau2.js
@@ -7,14 +7,45 @@ export default class niveau2 extends Phaser.Scene {
             key: "niveau2" //  ici on précise le nom de la classe en tant qu'identifiant
         });
     }
-    preload() { }
+    preload() {
+        // chargement tuiles de jeu
+        this.load.image("Phaser_tuilesdejeu", "src/assets/tuilesJeu.png");
+        // chargement de la carte
+        this.load.tilemapTiledJSON("carte", "src/assets/map.tmj");
+        this.load.spritesheet("img_perso", "src/assets/dude.png", {
+            frameWidth: 32,
+            frameHeight: 48
+        });
+        this.load.image('img_porte1', 'src/assets/door1.png');
+        this.load.image('img_porte2', 'src/assets/door2.png');
+        this.load.image('img_porte3', 'src/assets/door3.png');
+    }
 
     create() {
-        this.add.image(400, 300, "img_ciel");
-        this.groupe_plateformes = this.physics.add.staticGroup();
-        this.groupe_plateformes.create(200, 584, "img_plateforme");
-        this.groupe_plateformes.create(600, 584, "img_plateforme");
-        // ajout d'un texte distintcif  du niveau
+        const carteDuNiveau = this.add.tilemap("carte");
+        const tileset = carteDuNiveau.addTilesetImage(
+            "tuiles_de_jeu",
+            "Phaser_tuilesdejeu"
+        );
+        const calque_fond_noir = carteDuNiveau.createLayer(
+            "Calque fond noir",
+            tileset
+        );
+        const calque_eau = carteDuNiveau.createLayer(
+            "Calque de eau",
+            tileset
+        );
+        const calque_contours = carteDuNiveau.createLayer(
+            "Calque de contours",
+            tileset
+        );
+        const calque_plateforme = carteDuNiveau.createLayer(
+            "Calque plateforme",
+            tileset
+        );
+        calque_plateforme.setCollisionByProperty({ estSolide: true });
+
+        // ajout d'un texte distinctif du niveau
         this.add.text(400, 100, "Vous êtes dans le niveau 2", {
             fontFamily: 'Georgia, "Goudy Bookletter 1911", Times, serif',
             fontSize: "22pt"
@@ -24,14 +55,42 @@ export default class niveau2 extends Phaser.Scene {
         this.player.refreshBody();
         this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
+        this.player.setBounce(0.2);
+
+        // COLLISION avec les plateformes de la tilemap
+        this.physics.add.collider(this.player, calque_plateforme);
+
+        // CLAVIER
         this.clavier = this.input.keyboard.createCursorKeys();
         this.clavier.enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
-        this.physics.add.collider(this.player, this.groupe_plateformes);
+        
+        // animations
+        this.anims.create({
+            key: "anim_tourne_gauche",
+            frames: this.anims.generateFrameNumbers("img_perso", { start: 0, end: 3 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "anim_tourne_droite",
+            frames: this.anims.generateFrameNumbers("img_perso", { start: 5, end: 8 }),
+            frameRate: 10,
+            repeat: -1
+        });
+        this.anims.create({
+            key: "anim_face",
+            frames: [{ key: "img_perso", frame: 4 }],
+            frameRate: 20
+        });
+
+        this.physics.world.setBounds(0, 0, carteDuNiveau.widthInPixels, carteDuNiveau.heightInPixels);
+        this.cameras.main.setBounds(0, 0, carteDuNiveau.widthInPixels, carteDuNiveau.heightInPixels);
+        this.cameras.main.startFollow(this.player);
 
         // Initialisation du double saut
         this.nbSauts = 0;
         this.SAUT_MAX = 2;
-        this.porte_retour = this.physics.add.staticSprite(100, 550, "img_porte2"); 
+        this.porte_retour = this.physics.add.staticSprite(100, 550, "img_porte2");
     }
 
     update() {
@@ -46,17 +105,17 @@ export default class niveau2 extends Phaser.Scene {
             this.player.anims.play("anim_face");
         }
         // Saut / double saut
-        if (this.player.body.touching.down) {
+        if (this.player.body.onFloor()) {
             this.nbSauts = 0;
         }
         if (Phaser.Input.Keyboard.JustDown(this.clavier.space) && this.nbSauts < this.SAUT_MAX) {
             this.player.setVelocityY(-330);
             this.nbSauts++;
         }
-        if (Phaser.Input.Keyboard.JustDown(this.clavier.enter)== true) {
-      if (this.physics.overlap(this.player, this.porte_retour)) {
-            this.scene.switch("selection");
+        if (Phaser.Input.Keyboard.JustDown(this.clavier.enter) == true) {
+            if (this.physics.overlap(this.player, this.porte_retour)) {
+                this.scene.start("selection");
+            }
         }
     }
-}
 }
